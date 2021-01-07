90eb2b976e7842beae419f7ebbe97096617b904f
mwauraopenpath <allanmwaura2013@gmail.com>
2021-01-07 09:57:56 +0300

    changes

--- resources/views/clearance/index.blade.php
+++ resources/views/clearance/index.blade.php
@@ -7,7 +7,7 @@
                 <div class="card-header">
                     <div class="row">
                         <div class="col-8">
-                            <h4 class="card-title">Clients</h4>
+                            <h4 class="card-title">Clearance</h4>
                         </div>
                         <div class="col-4 text-right">
                             <a href="{{ url('print8')}}" class="btn btn-sm btn-primary">Clearance Form</a>
